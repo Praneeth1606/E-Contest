40592297353d61766cdb9509f9648d542352e241
Puneeth254 <111747004+Puneeth254@users.noreply.github.com>
2023-01-08 21:18:38 +0530

    Update application.py

--- app/application.py
+++ app/application.py
@@ -4,10 +4,10 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length, EqualTo
 from flask_session import Session
 import os
-from app.class_orm import db,User,Result,Submission
-import time
+#from app.class_orm import db,User,Result,Submission
+import time, pytz
 from datetime import datetime, timedelta
-from werkzeug import generate_password_hash,check_password_hash
+#from werkzeug import generate_password_hash,check_password_hash
 import threading
 import re
 import sys
@@ -15,6 +15,7 @@ from app.qnEvaluate import score
 from flask_socketio import SocketIO, emit
 import decimal 
 from sqlalchemy import nullslast
+from flask_sqlalchemy import SQLAlchemy
 
 sys.path.append('../evaluation')
 
@@ -26,18 +27,74 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=6)
 
 ENV = 'PROD'
+#ENV = 'dev'
 if ENV == 'dev' :
 	app.debug = True
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/e_contest'
+	#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwertyuiop@localhost:5432/my_db'
 else :
 	app.debug = False
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ofkyawpdjvlppl:e98959750f19e6517d4e8eabf5710d40bb4f7d5c1cb7b3ad0e645efee910acf3@ec2-174-129-234-111.compute-1.amazonaws.com:5432/dduv1ph7hquc21'
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'  #Enter your AWS server link here.
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+db = SQLAlchemy(app)
+#db.init_app(app)
+
+class User(db.Model) :
+	__tablename__ = "users"
+	id = db.Column(db.Integer,primary_key = True)
+	username = db.Column(db.String(20),nullable = False,unique = True)
+	password = db.Column(db.String,nullable = False)
+	name = db.Column(db.String(50))
+	contact = db.Column(db.String(20))
+	shaastraid = db.Column(db.String(25),nullable = False,unique = True)
+	email = db.Column(db.String(50))
+	done = db.Column(db.Boolean,nullable = False)
+	rem_time = db.Column(db.Integer,nullable = False)
+
+	def __init__(self, **kwargs) :
+	   	super(User, self).__init__(**kwargs)
+
+class Result(db.Model) :
+	__tablename__ = "results"
+	id = db.Column(db.Integer,primary_key = True)
+	userid = db.Column(db.Integer,db.ForeignKey('users.id'))
+	user = db.relationship("User",backref = "result",lazy = True)
+	q1s = db.Column(db.Integer)
+	q2s = db.Column(db.Integer)
+	q3s = db.Column(db.Integer)
+	q4s = db.Column(db.Integer)
+	q5s = db.Column(db.Integer)
+	q1t = db.Column(db.Numeric(precision = 14,scale = 4))
+	q2t = db.Column(db.Numeric(precision = 14,scale = 4))
+	q3t = db.Column(db.Numeric(precision = 14,scale = 4))
+	q4t = db.Column(db.Numeric(precision = 14,scale = 4))
+	q5t = db.Column(db.Numeric(precision = 14,scale = 4))
+	tot_score = db.Column(db.Integer)
+	tot_time = db.Column(db.Numeric(precision = 14,scale = 4))
+
+	def __init__(self, **kwargs) :
+		super(Result, self).__init__(**kwargs)
+
+class Submission(db.Model) :
+	__tablename__ = "submissions"
+	id = db.Column(db.Integer,primary_key = True)
+	userid = db.Column(db.Integer,db.ForeignKey('users.id'))
+	user = db.relationship("User",backref = "submission",lazy = True)
+	qnno = db.Column(db.Integer)
+	mark = db.Column(db.Integer)
+	message = db.Column(db.String)
+	timeofs = db.Column(db.Numeric(precision = 14,scale = 4))
+
+	def __init__(self, **kwargs) :
+		super(Submission, self).__init__(**kwargs)
+
+db.create_all()
 Session(app)
 socketio = SocketIO(app)
+
+
 
 class LoginForm(FlaskForm) :
 	username = StringField('username',validators = [InputRequired(), Length(min = 4,max = 20,message="Username must be between 4 and 20 characters")])
@@ -49,12 +106,43 @@ class SignupForm(FlaskForm) :
 	confirm_password = PasswordField('confirm_password',validators = [InputRequired(), Length(min = 6,max = 30,message='Password must be between 6 and 30 characters')])
 	email = StringField('email',validators = [Email(message='Not a valid Email Address'),Length(max = 50,message='Email must atmost 50 characters')])
 	name = StringField('name',validators = [Length(min = 1,max = 50,message='Name must be between 1 and 50 characters')])
-	shaastraID = StringField('shaastraID',validators = [InputRequired(), Length(max = 25,message='Shaastra ID must be atmost 25 characters')])
+	shaastraid = StringField('shaastraid',validators = [InputRequired(), Length(max = 25,message='Shaastra ID must be atmost 25 characters')])
 	contact = StringField('contact',validators = [Length(max = 20,message='Contact Number must be atmost 20 characters')])
 
 register_url = '/register'
 
 pno = 0
+
+IST = pytz.timezone('Asia/Kolkata')
+utc = pytz.utc
+
+startTime = datetime(2022,1,16,9,30,0) #Datetimes in UTC
+endTime = datetime(2022,1,16,11,30,0)
+
+startTime = utc.localize(startTime).astimezone(IST)
+endTime = utc.localize(endTime).astimezone(IST)
+
+def checkStartTime():
+	currTime = datetime.now(IST)
+	if(currTime<startTime):
+		return 1
+	return 0
+
+def checkEndTime():
+	currTime = datetime.now(IST)
+	if(currTime>endTime):
+		return 1
+	return 0
+
+def returnRemTime():
+	return int((endTime - datetime.now(IST)).total_seconds())
+
+def setRemTime():
+	usr = User.query.filter_by(id=session['userid']).first()
+	usr.remTime = int(returnRemTime())
+	db.engine.execute(f"update users set rem_time = {returnRemTime()} where id = {session['userid']}")
+	db.session.commit()
+	return
 
 @socketio.on('disconnect')
 def disconnect_user():
@@ -84,7 +172,11 @@ def add_header(r):
 
 @app.route('/',methods=['GET'])
 def index() :
-	return redirect('/login')
+	try:
+		session['userid']
+	except KeyError:
+		return redirect('/login')
+	return redirect('/dashboard')
 
 @app.route('/login',methods=['GET','POST'])
 def login() :
@@ -94,6 +186,18 @@ def login() :
 		form = LoginForm(request.form);
 
 		error = None
+		if checkStartTime():
+			#flash("You're early. The contest starts at " + startTime.strftime("%H:%M:%S %d/%m/%Y"))
+			error = "You're early. The contest starts at IST " + startTime.strftime("%H:%M:%S %d/%m/%Y")
+			return render_template('login.html', form=form, error=error)
+
+		if checkEndTime():
+			disconnect_user()
+			# flash("Contest ended at " + endTime.strftime("%H:%M:%S %d/%m/%Y"))
+			error = "Contest ended at IST " + endTime.strftime(
+				"%H:%M:%S %d/%m/%Y") + '\nFind standings at e-contest.herokuapp.com/standings'
+			return render_template('login.html', form=form, error=error)
+		# return redirect('/standings')
 		if (request.method == 'POST' and form.validate_on_submit()) :
 			user = User.query.filter_by(username = form.username.data).first()
 			if user is None :
@@ -107,7 +211,7 @@ def login() :
 					app.permanent_session_lifetime = timedelta(hours = 6)
 					session['username'] = user.username
 					session['userid'] = user.id
-					session['time'] = time.time()
+					session['time'] = returnRemTime()
 					user.done = True;
 					return redirect('/dashboard')
 			else :
@@ -128,6 +232,14 @@ def dashboard() :
 	except KeyError :
 		return redirect('/login')
 
+	setRemTime()
+
+	if checkEndTime():
+		disconnect_user()
+		# flash("Contest ended at " + endTime.strftime("%H:%M:%S %d/%m/%Y"))
+		error = "Contest ended at IST " + endTime.strftime(
+			"%H:%M:%S %d/%m/%Y") + '\nFind standings at e-contest.herokuapp.com/standings'
+		return render_template('login.html', form=LoginForm(), error=error)
 	if (request.method == 'POST' and ('quit' in request.form or 'remTime' in request.form)) :
 		usr = User.query.filter_by(id = session['userid']).first()
 		usr.done = True
@@ -139,25 +251,38 @@ def dashboard() :
 		return redirect('/login')
 
 	elif (request.method == 'POST' and 'code' in request.form) :
+		setRemTime()
+		if checkEndTime():
+			disconnect_user()
+			# flash("Contest ended at " + endTime.strftime("%H:%M:%S %d/%m/%Y"))
+			error = "Contest ended at IST " + endTime.strftime(
+				"%H:%M:%S %d/%m/%Y") + '\nFind standings at e-contest.herokuapp.com/standings'
+			return render_template('login.html', form=LoginForm(), error=error)
 		if Result.query.filter_by(userid = session['userid']).count() == 0 :
 			res = Result(userid = session['userid'])
 			db.session.add(res)
 			db.session.commit()
 		CODE = request.form.get('code')
 		qn = str(request.form.get('question-select'))
-		initTime = float(request.form.get('remtime'))
+		#initTime = float(request.form.get('remtime'))
+		initTime = returnRemTime()
+
+		res = 'Nothing yet'
 
 		@copy_current_request_context
 		def evaluate(code,qn,init_time) :
 			global pno
 			qn_no = str(re.sub('[^0-9]+',"",str(qn)))
 			res = score(code,qn_no,str(pno))
+
 			if (qn_no == '6') :
 				qn_no = '3'
 			elif (qn_no == '7') :
 				qn_no = '4'
+
 			currRes = Result.query.filter_by(userid = session['userid']).first()
 			if (res == 'CORRECT ANSWER') :
+				#flash(res)
 				if (qn == 'QN1') :
 					if currRes.q1s == 100 :
 						currRes.q1t = min([currRes.q1t,init_time])
@@ -210,12 +335,14 @@ def dashboard() :
 			pno += 1
 			currRes.tot_score = sum([e for e in scorel if e is not None])
 			currRes.tot_time = sum([decimal.Decimal(e) for e in timel if e is not None])
-			currRes.user.rem_time = 6000-init_time
+			#currRes.user.rem_time = 6000-init_time
+			currRes.user.rem_time = (endTime-startTime).total_seconds() - init_time
 
 			db.session.commit()
 
 
-		threading.Thread(target = evaluate,args = (CODE,qn,6000-initTime)).start()
+		#flash(res)
+		threading.Thread(target = evaluate,args = (CODE,qn,int((endTime-startTime).total_seconds()-initTime))).start()
 		flash('Solution Submitted Successfully')
 		return redirect('/dashboard')
 
@@ -238,13 +365,21 @@ def register() :
 	except KeyError :
 		error = None
 		form = SignupForm(request.form)
+		if checkEndTime():
+			disconnect_user()
+			# flash("Contest ended at " + endTime.strftime("%H:%M:%S %d/%m/%Y"))
+			error = "Contest ended at IST " + endTime.strftime(
+				"%H:%M:%S %d/%m/%Y") + '\nFind standings at e-contest.herokuapp.com/standings'
+			return render_template('login.html', form=LoginForm(), error=error)
 
 		if (request.method == 'POST' and form.validate_on_submit()) :
 			if User.query.filter_by(username = form.username.data).count() == 0 :
-				new_user = User(done = False,rem_time = 6000,username = form.username.data,password = form.confirm_password.data,email = form.email.data,shaastraID = form.shaastraID.data,name = form.name.data,contact = form.contact.data)
+				#new_user = User(done=False, rem_time=6000, username=form.username.data,password=form.confirm_password.data, email=form.email.data,shaastraid=form.shaastraid.data, name=form.name.data, contact=form.contact.data)
+				new_user = User(id=db.session.query(User).count()+1,done=False, rem_time=int((endTime-startTime).total_seconds()), username=form.username.data,password=form.confirm_password.data, email=form.email.data,shaastraid=form.shaastraid.data, name=form.name.data, contact=form.contact.data)
 				db.session.add(new_user)
-				db.session.commit() 
-				return '<h1>' + 'Successfully Registered Contestant' + '</h1>'
+				db.session.commit()
+				error='Successfully Registered Contestant'
+				return render_template('login.html', form=LoginForm(), error=error)
 			else :
 				error = "Username already Registered"
 
@@ -267,6 +402,12 @@ def submissions() :
 		session['userid'] 
 	except KeyError :
 		return redirect('/login')
+	if checkEndTime():
+		disconnect_user()
+		# flash("Contest ended at " + endTime.strftime("%H:%M:%S %d/%m/%Y"))
+		error = "Contest ended at IST " + endTime.strftime(
+			"%H:%M:%S %d/%m/%Y") + '\nFind standings at e-contest.herokuapp.com/standings'
+		return render_template('login.html', form=LoginForm(), error=error)
 	usr = User.query.filter_by(id = session['userid']).first()
 	subs = usr.submission
 	return render_template('submissions.html',name = session['username'],submissions = subs,to_time = time.strftime,to_ttuple = time.gmtime)
